e4c7ad394ef4f3f03d08f8f804e5a485933848e7
SHJoon <52848941+SHJoon@users.noreply.github.com>
2020-12-20 01:48:21 -0800

    Updating proper navs

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from 'react';
 import { Router, Link } from '@reach/router';
+import Nav from './components/Nav';
 import Main from './views/Main';
 
 function App() {
   return (
     <div className="App">
+      <Nav className="nav" />
       <Router>
         <Main path="/" />
       </Router>
