4116bc00e7c796a53856a7d71675c6f2e7a67206
SHJoon <52848941+SHJoon@users.noreply.github.com>
2020-12-24 15:18:55 -0800

    Updating about me page

--- client/src/components/AboutMe.js
+++ client/src/components/AboutMe.js
@@ -3,7 +3,8 @@ import React, { useState, useEffect } from 'react';
 const AboutMe = () => {
     return(
         <div>
-
+            <div>Full-Stack Developer|Software Engineer</div>
+            <div>Python|JavaScript|Java</div>
         </div>
     )
 }
