bdb71550479f5630cdd90ff0b9b389c715bea449
SHJoon <52848941+SHJoon@users.noreply.github.com>
2021-01-04 19:49:55 -0800

    Starting styling and added bootstrap

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { Link, navigate } from '@reach/router';
 import logo from '../images/logo.png';
+import styles from '../styles/Nav.module.css';
 
 const Nav = () => {
 
     return(
         <div>
-            <Link to="/"><img src={logo} alt="logo" className="logo" /></Link>
-            <div>
+            <Link to="/"><img className={ styles.logo } src={logo} alt="logo" /></Link>
+            <div className={ styles.links }>
                 <a href="https://github.com/SHJoon">GitHub </a>
                 <a href="https://www.linkedin.com/in/howard-shin/">Linkedin </a>
                 <Link to="/resume">Resume</Link>
