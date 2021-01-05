865b791b23c2f4fbcff9b47996a72e31e0c2ee34
SHJoon <52848941+SHJoon@users.noreply.github.com>
2021-01-05 14:58:49 -0800

    Adding profile pic

--- client/src/components/AboutMe.js
+++ client/src/components/AboutMe.js
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from 'react';
+
+import profile from '../images/profile.png';
+
 import styles from '../styles/AboutMe.module.css';
 
 const AboutMe = () => {
     return(
         <div className={ styles.intro }>
-            {/* <img src="" /> Add my face here */}
-            <p>Full-Stack Developer | Software Engineer</p>
-            <p>Python | JavaScript | Java</p>
-            <p>Hi, I'm Howard. I'm a highly motivated developer who loves computer and music.</p>
+            <h1>Full-Stack Developer | Software Engineer</h1>
+            <h3>Python | JavaScript | Java</h3>
+            <img className={ styles.pic } src={ profile } alt="profile" />
+            <h4>Hi, I'm Howard. I'm a highly motivated developer who loves computer and music.</h4>
         </div>
     )
 }
