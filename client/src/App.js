1b767535638762b25f58ed3a1b006b7f7ef88cbb
SHJoon <52848941+SHJoon@users.noreply.github.com>
2021-01-09 12:00:21 -0800

    Adding pdf annotations

--- client/src/App.js
+++ client/src/App.js
@@ -7,14 +7,22 @@ import Resume from './views/Resume';
 import Projects from './views/Projects';
 
 function App() {
-  const [ project1, setProject1 ] = useState({
+  const [ projects, setProjects ] = useState([{
     "title": "EasyShop",
     "language": "Python",
     "framework": "Django",
-    "description": <p>An e-commerce website built using Django, which featuers
-      secure user login/registration using bcrypt,
-    </p>
-  });
+    "description": `An e-commerce website built using Django, which featuers 
+    secure user login/registration using bcrypt`
+  }, {
+    "title": "AQI-Maps",
+    "language": "JavaScript",
+    "framework": "MERN Stack",
+    "description": `An user-friendly interactive website showcasing fluent 
+    implementation of Google Maps API and World Air Quality Index API. 
+    Users can view the air quality index of every available US stations with highly intuitive interface.`
+  }, {
+    "title": ""
+  }]);
   return (
     <div className="App">
       <Nav className="nav" />
--- client/src/views/Resume.js
+++ client/src/views/Resume.js
@@ -2,14 +2,15 @@ import React from 'react';
 import resume from '../documents/resume.pdf';
 import { Document, Page, pdfjs } from "react-pdf";
 import styles from '../styles/Resume.module.css';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 
 const Resume = () => {
     pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
     return(
-        <div className={ styles.resume }>
-            <Document file={resume}>
-                <Page object-fit="fill" pageNumber={1}></Page>
+        <div className={ styles.resume } >
+            <Document file={resume} size="object-fill">
+                <Page object-fit="fill" pageNumber={1} renderTextLayer={false}></Page>
             </Document>
         </div>
     )
