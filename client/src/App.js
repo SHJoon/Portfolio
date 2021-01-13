0875d2ee18a27c8e9d2abd1c3d104816ca74ccaa
SHJoon <52848941+SHJoon@users.noreply.github.com>
2021-01-13 00:37:26 -0800

    Adding extra images for carousel

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,34 @@ import meet from './images/meetup.png';
 import discordpy from './images/discordpy.png';
 import machinelearning from './images/machinelearning.png'
 
+import easyshop1 from './images/django/easyshop1.png';
+import easyshop2 from './images/django/easyshop2.png';
+import easyshop3 from './images/django/easyshop3.png';
+import easyshop4 from './images/django/easyshop4.png';
+import easyshop5 from './images/django/easyshop5.png';
+
+import aqi1 from './images/mern/aqi1.png';
+import aqi2 from './images/mern/aqi2.png';
+import aqi3 from './images/mern/aqi3.png';
+import aqi4 from './images/mern/aqi4.png';
+
+import meet1 from './images/spring/meet1.png';
+import meet2 from './images/spring/meet2.png';
+import meet3 from './images/spring/meet3.png';
+import meet4 from './images/spring/meet4.png';
+import meet5 from './images/spring/meet5.png';
+
+import discord1 from './images/discord/discord1.png';
+import discord2 from './images/discord/discord2.png';
+import discord3 from './images/discord/discord3.png';
+import discord4 from './images/discord/discord4.png';
+
+import nids1 from './images/nids/nids1.png';
+import nids2 from './images/nids/nids2.png';
+import nids3 from './images/nids/nids3.png';
+import nids4 from './images/nids/nids4.png';
+import nids5 from './images/nids/nids5.png';
+
 function App() {
 
   const projects = [{
@@ -22,7 +50,9 @@ function App() {
     "description": `An e-commerce website featuring 
     secure user login/registration using bcrypt, shopping cart per user, 
     review system, and more.`,
-    "prevImg": easyshop
+    "prevImg": easyshop,
+    "github": "https://github.com/SHJoon/EasyShop",
+    "images": [easyshop1, easyshop2, easyshop3, easyshop4, easyshop5]
   }, {
     "title": "AQI-Maps",
     "language": "JavaScript",
@@ -31,7 +61,9 @@ function App() {
     implementation of Google Maps API and World Air Quality Index API. 
     Users can view the air quality index of every available US stations 
     with highly intuitive interface.`,
-    "prevImg": aqi
+    "prevImg": aqi,
+    "github": "https://github.com/SHJoon/AQI-Maps",
+    "images": [aqi1, aqi2, aqi3, aqi4]
   }, {
     "title": "Let's Meet",
     "language": "Java",
@@ -39,7 +71,9 @@ function App() {
     "description": `A group meetup website where users can organize events
     across different locations that allows people with common interest to 
     socialize and come together.`,
-    "prevImg": meet
+    "prevImg": meet,
+    "github": "https://github.com/trinhgliedt/Let_s_Meet_Java_Project",
+    "images": [meet1, meet2, meet3, meet4, meet5]
   }, {
     "title": "InHouseBot",
     "language": "Python",
@@ -47,14 +81,18 @@ function App() {
     utilies, such as queue system in which hundreds of users can organize events, 
     currency/rating system to automatically keep track of players' performance, 
     and many more.`,
-    "prevImg": discordpy
+    "prevImg": discordpy,
+    "github": "https://github.com/SHJoon/ChongBot/tree/clean",
+    "images": [discord1, discord2, discord3, discord4]
   }, {
     "title": "Network Intrusion Detection",
     "language": "Python",
     "description": `A network intrusion detection project in which different 
     machine learning algorithms with different optimizations are employed to 
     detect anomaly vs normal network signal.`,
-    "prevImg": machinelearning
+    "prevImg": machinelearning,
+    "github": "https://github.com/SHJoon/Network_Intrusion_Detection",
+    "images": [nids1, nids2, nids3, nids4, nids5]
   }];
 
   useEffect(() => {
