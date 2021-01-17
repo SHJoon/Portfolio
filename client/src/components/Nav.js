e3e8e237dc275ef6169c2e10998ee5f638283482
SHJoon <52848941+SHJoon@users.noreply.github.com>
2021-01-17 15:46:51 -0800

    Fixing spacing

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -13,8 +13,8 @@ const Nav = () => {
             <Link className={ styles.logo } to="/"><img style={{width:"100px"}} src={logo} alt="logo" /></Link>
             <ul className={ styles.links }>
                 <li className= {styles.navLinks }><Link className={ styles.eachLinks } to="/resume">Resume</Link></li>
-                <li className= {styles.navLinks }><a className={ styles.eachLinks } href="https://github.com/SHJoon" target="_blank" rel="noopener noreferrer">GitHub </a></li>
-                <li className= {styles.navLinks }><a className={ styles.eachLinks } href="https://www.linkedin.com/in/howard-shin/" target="_blank" rel="noopener noreferrer">Linkedin </a></li>
+                <li className= {styles.navLinks }><a className={ styles.eachLinks } href="https://github.com/SHJoon" target="_blank" rel="noopener noreferrer">GitHub</a></li>
+                <li className= {styles.navLinks }><a className={ styles.eachLinks } href="https://www.linkedin.com/in/howard-shin/" target="_blank" rel="noopener noreferrer">Linkedin</a></li>
             </ul>
         </div>
     )
