1793807e29b8f17e7571faba1c0d87f810b0d620
SHJoon <52848941+SHJoon@users.noreply.github.com>
2021-01-30 14:42:42 -0800

    Adjusting animation

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ScrollAnimation from 'react-animate-on-scroll';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 
@@ -15,39 +16,43 @@ const Projects = (props) => {
             {
                 props.projectId !== -1 ?
                 <div>
-                    <Carousel
-                    className={ styles.selectedPics }
-                    dynamicHeight={true}
-                    emulateTouch={true}
-                    infiniteLoop={true}
-                    selectedItem={props.selectedProject}
-                    showArrows={ true }
-                    swiping={true}
-                    swipeable={true}
-                    >
-                    {
-                        props.projects[props.projectId].images.map((image, i) => {
-                            return(<div key={i}><img src={image} alt={props.projects[props.projectId].title}/></div>);
-                        })
-                    }
-                    </Carousel>
-                    <div className={ styles.projectDesc }>
-                        <h1>{ props.projects[props.projectId].title }</h1>
-                        <div className= { styles.langFrame }>
-                            <h3 className= { styles.lang }>Language: { props.projects[props.projectId].language }</h3>
-                            {props.projects[props.projectId].framework && <h3>Framework: { props.projects[props.projectId].framework }</h3>}
-                        </div>
-                        <p className= { styles.descParagraph }>{ props.projects[props.projectId].description }</p>
-                        <a
-                        className= { styles.githubBtn }
-                        href={ props.projects[props.projectId].github }
-                        target="_blank"
-                        rel="noopener noreferrer"
+                    <ScrollAnimation delay={1} animateIn="bounceInUp" duration={1} animateOnce={true}>
+                        <Carousel
+                        className={ styles.selectedPics }
+                        dynamicHeight={true}
+                        emulateTouch={true}
+                        infiniteLoop={true}
+                        selectedItem={props.selectedProject}
+                        showArrows={ true }
+                        swiping={true}
+                        swipeable={true}
                         >
-                            Check out the code!&nbsp;
-                            <img className={ styles.githubLogo } src={ github } alt="github" />
-                        </a>
-                    </div>
+                        {
+                            props.projects[props.projectId].images.map((image, i) => {
+                                return(<div key={i}><img src={image} alt={props.projects[props.projectId].title}/></div>);
+                            })
+                        }
+                        </Carousel>
+                    </ScrollAnimation>
+                    <ScrollAnimation delay={1} animateIn="bounceInUp" duration={1} animateOnce={true}>
+                        <div className={ styles.projectDesc }>
+                            <h1>{ props.projects[props.projectId].title }</h1>
+                            <div className= { styles.langFrame }>
+                                <h3 className= { styles.lang }>Language: { props.projects[props.projectId].language }</h3>
+                                {props.projects[props.projectId].framework && <h3>Framework: { props.projects[props.projectId].framework }</h3>}
+                            </div>
+                            <p className= { styles.descParagraph }>{ props.projects[props.projectId].description }</p>
+                            <a
+                            className= { styles.githubBtn }
+                            href={ props.projects[props.projectId].github }
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            >
+                                Check out the code!&nbsp;
+                                <img className={ styles.githubLogo } src={ github } alt="github" />
+                            </a>
+                        </div>
+                    </ScrollAnimation>
                 </div>
                 : <div></div>
             }
