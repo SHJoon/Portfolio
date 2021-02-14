173ba921fcf5ab853d27538555c3cfcc4e6cea3b
SHJoon <52848941+SHJoon@users.noreply.github.com>
2021-02-13 21:28:14 -0800

    Preparation for refactoring components

---
+++ client/src/components/ProjectSection.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import ProjectsPreview from './ProjectsPreview';
+import Projects from './Projects';
+
+const ProjectSection = () => {
+
+    return(
+        <div>
+            <ProjectsPreview />
+            <Projects />
+        </div>
+    )
+}
+
+export default ProjectSection;
