ef4683e3f76a49280316611b5e4c872880beb0e9
SHJoon <52848941+SHJoon@users.noreply.github.com>
2021-02-14 22:18:39 -0800

    Adding styling for projects

--- client/src/components/ProjectSection.js
+++ client/src/components/ProjectSection.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import ProjectsPreview from './ProjectsPreview';
 import Projects from './Projects';
+import styles from '../styles/ProjectSection.module.css';
 
 const ProjectSection = () => {
 
