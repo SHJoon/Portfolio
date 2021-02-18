0cebde4c0dcae6d8ff2afbfb520c51309f27228c
SHJoon <52848941+SHJoon@users.noreply.github.com>
2021-02-17 23:15:26 -0800

    Updating project section

--- client/src/components/ProjectSection.js
+++ client/src/components/ProjectSection.js
@@ -4,12 +4,35 @@ import ProjectsPreview from './ProjectsPreview';
 import Projects from './Projects';
 import styles from '../styles/ProjectSection.module.css';
 
+import ScrollAnimation from 'react-animate-on-scroll';
+
 const ProjectSection = () => {
 
     return(
         <div>
-            <ProjectsPreview />
-            <Projects />
+            <ScrollAnimation delay={1} animateIn="bounceInUp" duration={1} animateOnce={true}>
+                <h1 className="projectTitle">Projects</h1>
+                <p className="projectIntro">Cool programs I've worked on</p>
+            </ScrollAnimation>
+            <ScrollAnimation delay={1} animateIn="bounceInUp" duration={1} animateOnce={true}>
+                    <div className="row col-sm-12 project">
+                    {
+                        props.projects.map((project, i) => {
+                            return(<ProjectsPreview
+                                className="eachProject"
+                                projectId={ projectId }
+                                setProjectId={ setProjectId }
+                                key={i}
+                                project={ project }
+                                idx={i}
+                                />)
+                        })
+                    }
+                    </div>
+                </ScrollAnimation>
+            <ScrollAnimation delay={1} animateIn="bounceInUp" duration={1} animateOnce={true}>
+                <Projects projects={ props.projects } projectId={ projectId } />
+            </ScrollAnimation>
         </div>
     )
 }
